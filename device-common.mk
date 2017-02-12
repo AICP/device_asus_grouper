2022b00e4c0d3526ab8db76105466e094d3811ea
AndDiSa <anddisa@googlemail.com>
2017-02-12 11:46:30 +0100

    fixup healthd

--- device-common.mk
+++ device-common.mk
@@ -101,6 +101,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     sensors-config \
+    libhealthd.tegra3 \
     lights.grouper \
     audio.primary.grouper \
     power.grouper \
@@ -158,7 +159,6 @@ PRODUCT_COPY_FILES += \
     device/asus/grouper/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # NFCEE access control
